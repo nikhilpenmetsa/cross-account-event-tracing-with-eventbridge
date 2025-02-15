6afa56acebd8b9e43a9dada7b3e1d81df544eee0
Penmetsa <penmen@amazon.com>
2025-02-14 17:02:22 -0800

    lambda dynamodb stack in destination

---
+++ destination-account/src/app.py
@@ -0,0 +1,28 @@
+import os
+import json
+import uuid
+from datetime import datetime
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['TABLE_NAME'])
+
+def lambda_handler(event, context):
+    print(event)
+    try:
+        item = {
+            'id': str(uuid.uuid4()),
+            'timestamp': datetime.utcnow().isoformat(),
+            'message': event['detail']['requestData']['body'],
+            'eventDetails': event['detail']
+        }
+        
+        table.put_item(Item=item)
+        
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Event processed successfully')
+        }
+    except Exception as e:
+        print(f'Error: {str(e)}')
+        raise
